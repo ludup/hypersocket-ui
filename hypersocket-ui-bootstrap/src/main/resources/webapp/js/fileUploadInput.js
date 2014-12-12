cc608634ed4d93b74985aed9a179784b152ebc6d
Javier Izaguirre <javier.izaguirre@nervepoint.com>
2014-12-12 11:48:54 +0000

    ticket_327 progress bar added

--- hypersocket-ui-bootstrap/src/main/resources/webapp/js/fileUploadInput.js
+++ hypersocket-ui-bootstrap/src/main/resources/webapp/js/fileUploadInput.js
@@ -15,12 +15,27 @@ $.fn.fileUploadInput = function(data) {
 			+	'</div>'
 			+	'<div class="propertyValue col-xs-1 dialogActions">'
 			+	'	<a href="#" class="btn btn-primary" id="' + id + 'UploadButton"><i class="fa fa-upload"></i></a>'
+			+	'</div>'
+			+	'<div class="col-xs-12">'
+			+	'	<div id="' + id + 'UpdateProgressHolder" class="progress">'
+			+	'		<div id="' + id + 'UpdateProgress" class="progress-bar" role="progressbar"></div>'
+			+	'	</div>'
 			+	'</div>';
+	
 	$(this).append(html);
+	$('#' + id + 'UpdateProgressHolder').css('height', '12px');
+	$('#' + id + 'UpdateProgressHolder').hide();
 	
 	if(!options.showChooseFile){
 		$('#' + id + 'UploadButton').parent().hide();
 		$('#' + id + 'File').parent().removeClass('col-xs-11').addClass('col-xs-12');
+	}
+	
+	var uploadProgress = function(evt){
+		if (evt.lengthComputable) {
+			var width = Math.round(evt.loaded * 100 / evt.total);
+			$('#' + id + 'UpdateProgress').css("width", width + "%");
+		}
 	}
 	
 	var callback = {
@@ -50,6 +65,7 @@ $.fn.fileUploadInput = function(data) {
  							callback.remove();
  						});
  	 				}
+ 					$('#' + id + 'UpdateProgressHolder').hide();
  					if(options.disabled) {
  						callback.disable();
  					}
@@ -72,40 +88,40 @@ $.fn.fileUploadInput = function(data) {
  					bootbox.alert(getResource("fileUpload.emptyFileUploadInput.text"));
  					return;
  				}
+ 				$('#' + id + 'UpdateProgressHolder').show();
+ 				$('#' + id + 'UpdateProgress').css("width",  "0%");
  				var formData = new FormData();
  				formData.append('file', $('#' + id + 'File')[0].files[0]);
- 				$.ajax({
- 					type : 'POST',
- 					url : basePath + '/api/' +options.url,
- 					dataType : 'json',
- 					cache : false,
- 					contentType : false,
- 					processData : false,
- 					data : formData,
- 					success : function(data) {
- 						$('#' + id + 'File').parent().append('<span id="' + id + 'Info">' + data.resource.fileName + ', ' + data.resource.fileSize + ', ' + data.resource.md5Sum + '</span>');
+ 				
+ 		        var xhr = new XMLHttpRequest();
+ 		        xhr.upload.addEventListener("progress", uploadProgress, false);
+ 		        xhr.onreadystatechange=function()
+ 		        {
+ 		        	if (xhr.readyState==4 && xhr.status==200)
+ 		        	{
+ 		        		data = jQuery.parseJSON(xhr.response).resource;
+ 		        		$('#' + id + 'File').parent().append('<span id="' + id + 'Info">' + data.fileName + ', ' + data.fileSize + ', ' + data.md5Sum + '</span>');
  						$('#' + id + 'File').remove();
  						$('#' + id + 'UploadButton').parent().append('<a class="btn btn-danger" id="' + id + 'RemoveButton"><i class="fa fa-trash-o"></i></a>');
  						$('#' + id + 'UploadButton').remove();
- 						$('#' + id + 'Info').data('uuid', data.resource.name);
+ 						$('#' + id + 'Info').data('uuid', data.name);
  						$('#' + id + 'RemoveButton').click(function(){
  							callback.remove();
  						});
  						if(options.disabled) {
  							callback.disable();
  						}
- 					},
- 					error: function(jqXHR, textStatus, errorThrown) {
- 						bootbox.alert(errorThrown);
- 					}
- 				});
+ 		        	}
+ 		        }
+ 		        xhr.open("POST", basePath + '/api/' +options.url);
+ 		        xhr.send(formData);
  			},
  			remove: function() {
  				if(!$('#' + id + 'Info').length){
  					bootbox.alert(getResource("fileUpload.noExistingFile.text"));
  					return;
  				}
- 				
+ 				$('#' + id + 'UpdateProgressHolder').hide();
  				deleteJSON(options.url + '/' + $('#' + id + 'Info').data('uuid'), null, function(data){
  					$('#' + id + 'Info').parent().append('<input type="file" id="' + id + 'File"/>');
 					$('#' + id + 'Info').remove();
