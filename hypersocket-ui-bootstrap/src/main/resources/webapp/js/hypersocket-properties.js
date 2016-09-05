ce7ac9092e051a7cae2c0962adba00a30b546829
ludup <lee@ludup.com>
2016-09-05 16:34:45 +0100

    Added uuid validation

--- hypersocket-ui-bootstrap/src/main/resources/webapp/js/hypersocket-properties.js
+++ hypersocket-ui-bootstrap/src/main/resources/webapp/js/hypersocket-properties.js
@@ -292,7 +292,12 @@ function validateAny(option,value){
 					matched = true;
 				}
 				break;
-			default : matched = false;
+			case 'uuid':
+				matched = validateRegex(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i, value);
+				break;
+			default : 
+				matched = false;
+			break;
 		}
 		if(matched){
 			break;
@@ -366,7 +371,10 @@ function validateAll(option,value){
 				} else {
 					matched = false;
 				}
-			    break;    
+			    break;  
+			case 'uuid':
+				matched = validateRegex(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i, value);
+				break;
 			default : matched = false;
 		}
 		if(!matched){
