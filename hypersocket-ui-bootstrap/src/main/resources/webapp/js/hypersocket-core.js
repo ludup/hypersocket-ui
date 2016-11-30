e30af6df63a1ef500a9263fd7f938796a4cbefbf
ludup <lee@ludup.com>
2016-11-30 12:03:05 +0000

    Stop showing error on session/peek failure.

--- hypersocket-ui-bootstrap/src/main/resources/webapp/js/hypersocket-core.js
+++ hypersocket-ui-bootstrap/src/main/resources/webapp/js/hypersocket-core.js
@@ -471,9 +471,11 @@ function home(data) {
 			log("Checking session timeout");
 			
 			getJSON('session/peek', null, function(data) {
-				setTimeout(checkTimeout, 30000);
+				if(data.success) {
+					setTimeout(checkTimeout, 30000);
+				}
 			}, function() {
-				return !hasShutdown;
+				return false;
 			});
 		};
 		
