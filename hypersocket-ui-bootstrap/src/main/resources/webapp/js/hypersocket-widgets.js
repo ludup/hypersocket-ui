0726e442d91bdb778a76f785ebbf75e4b8c5b6c5
Javier Izaguirre <javier@hypersocket.com>
2017-07-21 12:09:09 +0100

    2.2_1425 #1425 - File selection widget for virtual file system Widget working but needs more styling

--- hypersocket-ui-bootstrap/src/main/resources/webapp/js/hypersocket-widgets.js
+++ hypersocket-ui-bootstrap/src/main/resources/webapp/js/hypersocket-widgets.js
@@ -5605,4 +5605,246 @@ $.fn.roles = function(data) {
 	$(this).data('widget', callback);
 	$(this).addClass('widget');
 	return callback;
-} 
+}
+
+$.fn.fileTree = function(data) {
+	var id = $(this).attr('id');
+	
+	var include = function(node){
+			
+		if(node.children && node.children.length){
+			for(var index = 0; index < node.children.length; index++){
+				include($('#' + id + 'TreeView').jstree().get_node(node.children[index]));
+			}
+		}else{
+			var fullPath = node.text;
+			for(var index = 0; index < node.parents.length - 2; index++){
+				fullPath = $('#' + id + 'TreeView').jstree().get_node(node.parents[index]).text + '/' + fullPath;
+			}
+			addFullPath(fullPath);
+		}
+	}
+	
+	var addFullPath = function(fullPath){
+		var found = false;
+		
+		$('#' + id + 'Included option').each(function(index, option){
+			if($(option).attr('value') == fullPath){
+				found = true;
+				return false;
+			}else if($(option).attr('value') > fullPath){
+				$(option).before('<option value="' + fullPath + '">' + fullPath + '</option>');
+				found = true;
+				return false;
+			}
+		});
+		if(!found){
+			$('#' + id + 'Included').append('<option value="' + fullPath + '">' + fullPath + '</option>');
+		}
+	}
+
+	if ($(this).data('created')) {
+		options = $(this).widget().options();
+		
+		return;
+
+	} else {
+
+		var options = $.extend(
+		{	valueAttr : 'id', 
+			nameAttr : 'name', 
+			nameIsResourceKey : false, 
+			disabled : false,
+			fileTreeResourceKey: 'text.fileTree',
+			includedLabelResourceKey: 'text.included',
+			getUrlData: function(data) {
+				return data;
+			}
+		}, data);
+
+
+		var callback = {
+				setValue: function(val) {
+					if(Object.prototype.toString.call(val) === '[object Array]'){
+						$.each(val, function(index, fullPath){
+							addFullPath(fullPath);
+						});
+					}else if(typeof val === 'string'){
+						addFullPath(fullPath);
+					}
+				},
+				getValue: function() {
+					var values = []
+					$('#' + id + 'Included option').each(function(index, option){
+						values.push($(option).val());
+					});
+					return values;
+				},
+				reset: function() {
+					
+				},
+				disable: function() {
+					options.disabled = true;
+					$('#' + id + 'Included').attr('disabled', true);
+					$('#' + id + 'Include').attr('disabled', 'disabled');
+					$('#' + id + 'IncludeAll').attr('disabled', 'disabled');
+					$('#' + id + 'Exclude').attr('disabled', 'disabled');
+					$('#' + id + 'ExcludeAll').attr('disabled', 'disabled');
+				},
+				enable: function() {
+					options.disabled = false;
+					$('#' + id + 'Included').attr('disabled', false);
+					$('#' + id + 'Include').removeAttr('disabled');
+					$('#' + id + 'IncludeAll').removeAttr('disabled');
+					$('#' + id + 'Exclude').removeAttr('disabled');
+					$('#' + id + 'ExcludeAll').removeAttr('disabled');
+				},
+				options: function() {
+					return options;
+				},
+				getInput: function() {
+					return $('#' + id);
+				},
+	 			clear: function() {
+	 				$('#' + id + 'Included option').remove();
+	 			}
+		};
+		
+		$('#' + id).append(
+					'<div class="row">'
+				+	'	<div style="height:400px;" class="col-md-6">'
+				+	'		<label>Excluded</label>'
+				+	'		<div class="panel panel-body" id="' + id + 'TreeView" style="height:100%;"></div>'
+				+	'	</div>'
+				+	'	<div class="col-md-1" style="padding:15px;">'
+				+	'		<button id="' + id + 'Include" class="btn btn-small btn-primary"><i class="fa fa-chevron-right"></i></button>'
+				+	'		<button id="' + id + 'IncludeAll" class="btn btn-small btn-primary"><i class="fa fa-chevron-right"></i><i class="fa fa-chevron-right"></i></button>'
+				+	'		<button id="' + id + 'Exclude" class="btn btn-small btn-primary"><i class="fa fa-chevron-left"></i></button>'
+				+	'		<button id="' + id + 'ExcludeAll" class="btn btn-small btn-primary"><i class="fa fa-chevron-left"></i><i class="fa fa-chevron-left"></i></button>'
+				+	'	</div>'
+				+	'	<div style="height:400px;" class="col-md-5">'
+				+	'		<label>Excluded</label>'
+				+	'		<select id="' + id + 'Included" multiple="multiple" class="panel-body formInput text form-control" style="height:100%;padding:5px;">'
+				+	'		</select>'
+				+	'	</div>'
+				+	'</div>');
+		
+		if(options.values && options.values.length){
+			$.each(options.values, function(index, value){
+				$('#' + id + 'Included').append('<option value="' + value + '">' + value + '</option>');
+			});
+			
+			if(options.changed) {
+	 			options.changed(callback);
+	 		}
+		}
+		
+		$('#' + id + 'Include').click(function(){
+			if(!options.disabled){
+				var selected = $('#' + id + 'TreeView').jstree('get_selected', true);
+				$.each(selected, function(index, node){
+					include(node);
+				});
+			}
+		});
+		
+		$('#' + id + 'IncludeAll').click(function(){
+			if(!options.disabled){
+				include($('#' + id + 'TreeView').jstree().get_node('#'));
+			}
+		});
+		
+		$('#' + id + 'Exclude').click(function(){
+			if(!options.disabled){
+				var selected = $('#' + id + 'Included').val();
+				if(selected && selected.length){
+					$('#' + id + 'Included option').each(function(index, option){
+						if(selected.indexOf($(option).val()) != -1){
+							$(option).remove();					
+						}
+					});
+				}
+			}
+		});
+		
+		$('#' + id + 'ExcludeAll').click(function(){
+			if(!options.disabled){
+				$('#' + id + 'Included option').remove();
+			}
+		});
+		
+		var name = (options && options.resourceKey != null ) ? formatResourceKey(options.resourceKey) : id ;
+		
+		$('#' + id + 'TreeView').jstree({
+			"core" : {
+				"animation" : 0,
+			    "multiple" : true,
+			    "themes" : { "stripes" : false },
+			    'data' : function(obj, cb) {
+			    	getJSON(options.url, null, function(data) {
+			    		cb.call(this, data);
+			    	});
+			    } 
+			},
+			"types" : {
+		  		"folder" : {
+		  			"icon" : "fa fa-2x fa-folder-o"
+		  		},
+		  		"pdf" : {
+		  			"icon" : "fa fa-2x fa-file-pdf-o"
+		  		},
+		  		"xls" : {
+		  			"icon" : "fa fa-2x fa-file-excel-o"
+		  		},
+		  		"mp3" : {
+		  			"icon" : "fa fa-2x fa-file-sound-o"
+		  		},
+		  		"doc" : {
+		  			"icon" : "fa fa-2x fa-file-word-o"
+		  		},
+		  		"zip" : {
+		  			"icon" : "fa fa-2x fa-file-archive-o"
+		  		},
+		  		"png" : {
+		  			"icon" : "fa fa-2x fa-file-image-o"
+		  		},
+		  		"txt" : {
+		  			"icon" : "fa fa-2x fa-file-text-o"
+		  		},
+		  		"js" : {
+		  			"icon" : "fa fa-2x fa-file-code-o"
+		  		},
+		  		"ppt" : {
+		  			"icon" : "fa fa-2x fa-file-powerpoint-o"
+		  		},
+		  		"mp4" : {
+		  			"icon" : "fa fa-2x fa-file-video-o"
+		  		},
+		  		"default" : {
+		  			"icon" : "fa fa-2x fa-file-file-o"
+		  		}
+			},
+			
+			"plugins" : [
+				"contextmenu", "crrm", "types"
+			]
+		});
+		
+		$('#' + id + 'TreeView').on("open_node.jstree", function (e, data) {
+			data.instance.set_icon(data.node, 'fa fa-2x fa-folder-open-o');
+		});
+		$('#' + id + 'TreeView').on("close_node.jstree", function (e, data) {
+			data.instance.set_icon(data.node, 'fa fa-2x fa-folder-o');
+		});
+	}
+
+	if(options.disabled) {
+		callback.disable();
+	}
+	
+	$(this).data('created', true);
+	$(this).data('widget', callback);
+	$(this).addClass('widget');
+	return callback;
+
+};
