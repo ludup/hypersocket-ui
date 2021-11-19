b637c1475d08d121b1a877b9830a8d1b69d48a53
Lee Painter <lee@javassh.com>
2021-11-19 12:35:20 +0000

    Added new finished option for wizards so they can move page at the end of the flow.

--- hypersocket-ui-bootstrap/src/main/resources/webapp/js/hypersocket-widgets.js
+++ hypersocket-ui-bootstrap/src/main/resources/webapp/js/hypersocket-widgets.js
@@ -5720,6 +5720,9 @@ $.fn.wizardPage = function(data) {
 							$('#button' + idx).find('span').text(getResource(options.doneText));
 						} else {
 							$('#button' + idx).attr('disabled', true);
+							if(options.finished) {
+								options.finished();
+							}
 						}
 
 						$('.backButton').attr('disabled', true);
