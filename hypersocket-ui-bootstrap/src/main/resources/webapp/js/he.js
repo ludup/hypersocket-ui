18e1207aec58ed7867ea77353e4191f05c584919
Lee Painter <lee@javassh.com>
2022-01-30 18:25:21 +0000

    Last change did not work as it requires a string type too that has replace function on. Now check for nullness and string.replace exists.

--- hypersocket-ui-bootstrap/src/main/resources/webapp/js/he.js
+++ hypersocket-ui-bootstrap/src/main/resources/webapp/js/he.js
@@ -313,13 +313,13 @@
 	};
 
 	var escape = function(string) {
-		if(string) {
-			return string.replace(regexEscape, function($0) {
-				// Note: there is no need to check `has(escapeMap, $0)` here.
-				return escapeMap[$0];
-			});
+		if(!string || !string.replace) {
+			return string;
 		}
-		return string;
+		return string.replace(regexEscape, function($0) {
+			// Note: there is no need to check `has(escapeMap, $0)` here.
+			return escapeMap[$0];
+		});
 	};
 
 	/*--------------------------------------------------------------------------*/
